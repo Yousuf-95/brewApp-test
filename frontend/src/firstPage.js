7d55f69a746b45cbfac00b12317fb9595c7c48e9
Yousuf Suhail <yf.yousuf95@gmail.com>
2022-03-03 20:45:22 +0530

    Added active link style

--- frontend/src/firstPage.js
+++ frontend/src/firstPage.js
@@ -11,6 +11,7 @@ import { AiFillFire } from 'react-icons/ai';
 const FirstPage = () => {
 
     let [myCards, setmyCards] = useState([]);
+    let [activeTab, setactiveTab] = useState(0);
 
     useEffect(() => {
         const getCards = async () => {
@@ -22,13 +23,11 @@ const FirstPage = () => {
         getCards();
     }, []);
 
-    const handleClick = async (e, sortby) => {
-
+    const handleClick = async (sortby) => {
         const fetchResult = await fetch('https://fierce-bastion-54100.herokuapp.com/api/getCards/?sortby=' + sortby);
         const cards = await fetchResult.json();
         setmyCards(cards);
     };
-
 
     return (
         
@@ -39,13 +38,13 @@ const FirstPage = () => {
                 </div>
 
                 <ul>
-                    <li><span><AiFillFire /></span>24h Trending</li>
-                    <li onClick={(e) => handleClick(e,'latest')} >Latest Shows </li>
-                    <li onClick={(e) => handleClick(e,'popular')}>Most Popular </li>
-                    <li><span><IoDiamond /></span> In Genesis </li>
-                    <li><span><GiByzantinTemple /></span>In Temple </li>
-                    <li><span><GiWhirlwind /></span>In Void</li>
-                    <li><span><GiGorilla /></span>#BAYC</li>  
+                    <li className={(activeTab === 0) ? `${styles.active}` : ''} onClick={() => setactiveTab(0)}><span><AiFillFire /></span>24h Trending</li>
+                    <li className={(activeTab === 1) ? `${styles.active}` : ''} onClick={() => {setactiveTab(1); handleClick('latest');}} >Latest Shows </li>
+                    <li className={(activeTab === 2) ? `${styles.active}` : ''} onClick={() => {setactiveTab(2); handleClick('popular');}}>Most Popular </li>
+                    <li className={(activeTab === 3) ? `${styles.active}` : ''} onClick={() => setactiveTab(3)}><span><IoDiamond /></span> In Genesis </li>
+                    <li className={(activeTab === 4) ? `${styles.active}` : ''} onClick={() => setactiveTab(4)}><span><GiByzantinTemple /></span>In Temple </li>
+                    <li className={(activeTab === 5) ? `${styles.active}` : ''} onClick={() => setactiveTab(5)}><span><GiWhirlwind /></span>In Void</li>
+                    <li className={(activeTab === 6) ? `${styles.active}` : ''} onClick={() => setactiveTab(6)}><span><GiGorilla /></span>#BAYC</li>  
                 </ul>
                 
 
